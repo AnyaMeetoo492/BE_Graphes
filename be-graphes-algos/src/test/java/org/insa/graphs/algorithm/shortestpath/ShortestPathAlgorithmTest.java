651fa891b5414dcf98d2b9897a891a12e9d1e8f0
Anya Meetoo <anyameetoo@gmail.com>
2024-05-26 14:48:12 +0200

    test

--- be-graphes-algos/src/test/java/org/insa/graphs/algorithm/shortestpath/ShortestPathAlgorithmTest.java
+++ be-graphes-algos/src/test/java/org/insa/graphs/algorithm/shortestpath/ShortestPathAlgorithmTest.java
@@ -8,7 +8,7 @@ import org.insa.graphs.model.Graph;
 //import org.insa.graphs.model.Path;
 import org.insa.graphs.model.io.BinaryGraphReader;
 import org.insa.graphs.model.io.GraphReader;
-//import org.junit.Assume;
+import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -40,8 +40,8 @@ public abstract class ShortestPathAlgorithmTest {
     Collection<Object> data = new ArrayList<>();
 
     // LOAD GRAPH
-    final Graph insa = read("/mnt/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Maps/insa.mapgr");
-    final Graph belgium = read("/mnt/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Maps/belgium.mapgr");
+    final Graph insa = read("C:/Users/anya/Pictures/BE_Graphes/insa.mapgr");
+    final Graph belgium = read("C:/Users/anya/Pictures/BE_Graphes/belgium.mapgr");
 
     // load data
     //ShortestPathData(Graph graph, Node origin, Node destination, ArcInspector arcInspector)
@@ -81,42 +81,50 @@ public abstract class ShortestPathAlgorithmTest {
   //Verify that solution path is valid
   @Test
   public void VerifyValidity(){
+    Assume.assumeTrue(this.solution.isFeasible());
     assertTrue(this.solution.getPath().isValid());
   }
 
   //Verify that origin solution is origin data given
   @Test
   public void VerifyOrigin(){
+    Assume.assumeTrue(this.solution.isFeasible());
     assertEquals(this.data.getOrigin(), this.solution.getInputData().getOrigin());
   }
 
   //Verify that destination solution is destination data given
   @Test
   public void VerifyDestination(){
+    Assume.assumeTrue(this.solution.isFeasible());
     assertEquals(this.data.getDestination(), this.solution.getInputData().getDestination());
   }
 
   //Verify that solution is equal to solution given by Bellman Ford Algorithm
   @Test
   public void VerifyEqualsBellmanFord(){
+    Assume.assumeTrue(this.data.getGraph().getNodes().size() <= 5000);
+
     BellmanFordAlgorithm bellmanfordAlgo = new BellmanFordAlgorithm(this.data);
     ShortestPathSolution bellmanfordSolution = bellmanfordAlgo.run();
 
+    assertSame(bellmanfordSolution.getStatus(), this.solution.getStatus());
+
+    if (!this.solution.isFeasible()) return;
+    
+    //verify if solution has same length as Bellman Ford Solution
+    assertSame(this.solution.getPath().getLength(), bellmanfordSolution.getPath().getLength());
     //verify both solution are feasible or not
     assertSame(this.solution.isFeasible(), bellmanfordSolution.isFeasible());
 
-    if (this.solution.isFeasible()){
-      //verify if solution has same length as Bellman Ford Solution
-      assertSame(this.solution.getPath().getLength(), bellmanfordSolution.getPath().getLength());
-      assertSame(this.solution.getPath().getArcs().size(), bellmanfordSolution.getPath().getArcs().size());
-
-      //Verify that each arc of soltion is the same as arc of Bellman Ford Solution
-      for (int i = 0; i < this.solution.getPath().getArcs().size(); i++) {
-        assertSame(this.solution.getPath().getArcs().get(i).getDestination(), bellmanfordSolution.getPath().getArcs().get(i).getDestination());
-      }
+    if (!this.solution.isFeasible()) {
+      return;
     }
-    else{
-      return; // solution unFeasible
+
+    assertSame(this.solution.getPath().getArcs().size(), bellmanfordSolution.getPath().getArcs().size());
+
+    //Verify that each arc of soltion is the same as arc of Bellman Ford Solution
+    for (int i = 0; i < this.solution.getPath().getArcs().size(); i++) {
+      assertSame(this.solution.getPath().getArcs().get(i).getDestination(), bellmanfordSolution.getPath().getArcs().get(i).getDestination());
     }
 
   }
